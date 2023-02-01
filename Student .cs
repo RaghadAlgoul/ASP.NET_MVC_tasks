b079e2e6b29ceec796a0ba19721d2471fbc00982
Raghad Alghoul <102190199+RaghadAlgoul@users.noreply.github.com>
2023-02-01 19:21:09 +0200

    2-1

---
+++ Student .cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Task2.Models
+{
+    public class Student
+    {
+        public int ID { set; get; }
+        public String Name { set; get; }
+        public string Major { get; set; }
+        public string Faculty { get; set; }
+    }
+}
